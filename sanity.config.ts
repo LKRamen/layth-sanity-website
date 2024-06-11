b0dd1fcb44f9ee1e6fae02a3ba1b121514cece71
Layth Rahman <laythrahman11@gmail.com>
2024-06-11 14:33:19 -0400

    added structure and reconfigured the config file

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,15 +2,16 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
+import {structure} from './structure'
 
-export default defineConfig({
-  name: 'default',
+const config = defineConfig({
+  name: 'config',
   title: 'layth-sanity-website',
 
   projectId: '6a6g9fpr',
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [structureTool({structure}), visionTool()],
 
   schema: {
     types: schemaTypes,
