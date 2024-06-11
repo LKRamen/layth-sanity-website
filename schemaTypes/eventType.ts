043ed78b38eee681be820fb17cc7bb31d0f9ddb3
Layth Rahman <laythrahman11@gmail.com>
2024-06-11 12:54:22 -0400

    Added eventType

---
+++ schemaTypes/eventType.ts
@@ -0,0 +1,14 @@
+import {defineField, defineType} from 'sanity'
+
+
+export const eventType = defineType({
+  name: 'event',
+  title: 'Event',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'name',
+      type: 'string',
+    }),
+  ],
+})
